a8d6b397f71fce89d9ad0711900188431def3439
Pradyun Narkadamilli <pradyungn@gmail.com>
2022-06-24 15:29:29 -0700

    Added README, added confirmation endpoint

--- userAPI.py
+++ userAPI.py
@@ -14,10 +14,25 @@ client = boto3.client("cognito-idp", region_name="us-east-2")
 
 @app.route("/signup", methods=['POST'])
 def sign_up():
-    print(request.form)
+    # TODO - sanitize signup paramaters
+    #   - check for valid email and password
     return client.sign_up(ClientId=environ["COGNITO_CLIENT_ID"],
                     Username=request.json["user"],
                     Password=request.json["pass"])
+
+@app.route("/confirm", methods=['POST'])
+def confirm():
+
+    if request.json is None:
+        return "No JSON load supplied", 400
+
+
+    if not ("user" in request.json.keys() and "code" in request.json.keys()):
+        return "Required fields not supplied", 400
+
+    return client.confirm_sign_up(ClientId=environ["COGNITO_CLIENT_ID"],
+                             Username=request.json["user"],
+                             ConfirmationCode=request.json["code"])
 
 
 app.run("0.0.0.0", 3000, load_dotenv=True)
