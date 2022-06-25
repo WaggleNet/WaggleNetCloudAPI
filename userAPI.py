b55e952dc2eb65509f7282934f6eaa5a3ae6c601
Pradyun Narkadamilli <pradyungn@gmail.com>
2022-06-24 19:14:12 -0700

    Done with Cognito functions (for the most part)

--- userAPI.py
+++ userAPI.py
@@ -8,31 +8,70 @@ import boto3
 
 # initialize Flask client
 app = Flask(__name__)
-
 # initializing client w/ boto3
 client = boto3.client("cognito-idp", region_name="us-east-2")
 
-@app.route("/signup", methods=['POST'])
+@app.route("/signUp", methods=['POST'])
 def sign_up():
     # TODO - sanitize signup paramaters
     #   - check for valid email and password
+    #   - Exception behavior
+    if request.json is None:
+        return "No JSON payload supplied", 400
+
     return client.sign_up(ClientId=environ["COGNITO_CLIENT_ID"],
                     Username=request.json["user"],
                     Password=request.json["pass"])
 
 @app.route("/confirm", methods=['POST'])
 def confirm():
-
     if request.json is None:
         return "No JSON load supplied", 400
-
 
     if not ("user" in request.json.keys() and "code" in request.json.keys()):
         return "Required fields not supplied", 400
 
-    return client.confirm_sign_up(ClientId=environ["COGNITO_CLIENT_ID"],
-                             Username=request.json["user"],
-                             ConfirmationCode=request.json["code"])
+    # TODO - validate if username is an email. Make helper function for this?
 
+    try:
+        return client.confirm_sign_up(ClientId=environ["COGNITO_CLIENT_ID"],
+                                Username=request.json["user"],
+                                ConfirmationCode=str(request.json["code"]))
+    except Exception:
+        return "Uncaught internal server error", 400
+
+@app.route("/resend", methods=["POST"])
+def resend():
+    if request.json is None:
+        return "No JSON load supplied", 400
+
+    if "user" not in request.json.keys():
+        return "username not supplied", 400
+
+    try:
+        return client.resend_confirmation_code(ClientId=environ["COGNITO_CLIENT_ID"],
+                                        Username=request.json["user"])
+    except:
+        return "Uncaught internal server error", 400
+
+@app.route("/signIn", methods=["POST"])
+def signIn():
+    # TODO - validate/sanitize username and password
+    if request.json is None:
+        return "No JSON load supplied", 400
+
+    if not ("user" in request.json.keys() and "pass" in request.json.keys()):
+        return "Required fields not supplied", 400
+
+    try:
+        response =  client.initiate_auth(ClientId=environ["COGNITO_CLIENT_ID"],
+                                AuthFlow="USER_PASSWORD_AUTH",
+                                AuthParameters={"USERNAME": request.json["user"],
+                                                "PASSWORD": request.json["pass"]})
+
+        print(response.keys())
+        return {k:response["AuthenticationResult"][k] for k in ("AccessToken", "IdToken")}
+    except client.exceptions.NotAuthorizedException:
+        return "Incorrect username or password", 400
 
 app.run("0.0.0.0", 3000, load_dotenv=True)
